e0237b5a5c415a49a6323020039ecdbaa27b07a1
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2025-04-22 19:31:30 +0100

    progress

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
@@ -12,6 +13,12 @@ import (
 
 var db *sql.DB
 var tmpl *template.Template
+
+type Task struct {
+	Id       int
+	TaskName string
+	Done     bool
+}
 
 func init() {
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
@@ -38,7 +45,11 @@ func main() {
 	gRouter := mux.NewRouter()
 
 	gRouter.HandleFunc("/", HomeHandler)
+	gRouter.HandleFunc("/tasks", fetchTasks).Methods("GET")
+	gRouter.HandleFunc("/new", newForm)
+	gRouter.HandleFunc("/add", addTask).Methods("POST")
 
+	fmt.Println("Listening on Port 3030")
 	http.ListenAndServe(":3000", gRouter)
 }
 
@@ -48,4 +59,66 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error executing template: "+err.Error(),
 			http.StatusInternalServerError)
 	}
+}
+
+func fetchTasks(w http.ResponseWriter, r *http.Request) {
+	tasks, err := getTasks(db)
+	if err != nil {
+		http.Error(w, "Error fetching tasks: "+err.Error(), http.StatusInternalServerError)
+	}
+
+	tmpl.ExecuteTemplate(w, "todoList", tasks)
+}
+
+func getTasks(dbPointer *sql.DB) ([]Task, error) {
+	query := "SELECT id, task, done FROM tasks;"
+	rows, err := dbPointer.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []Task
+
+	for rows.Next() {
+		var todo Task
+
+		rowErr := rows.Scan(&todo.Id, &todo.TaskName, &todo.Done)
+		if rowErr != nil {
+			return nil, rowErr
+		}
+
+		tasks = append(tasks, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func newForm(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "taskAddForm", nil)
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	task := r.FormValue("task")
+
+	query := "INSERT INTO tasks (task) VALUES (?);"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, execErr := stmt.Exec(task)
+	if execErr != nil {
+		log.Fatal(err)
+	}
+
+	todos, _ := getTasks(db)
+
+	tmpl.ExecuteTemplate(w, "todoList", todos)
 }
