b5a02b2c3365a12a5ba5d2d052dc9932a8738d5d
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2025-04-23 16:37:31 +0100

    update and remove tasks

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -48,6 +50,9 @@ func main() {
 	gRouter.HandleFunc("/tasks", fetchTasks).Methods("GET")
 	gRouter.HandleFunc("/new", newForm)
 	gRouter.HandleFunc("/add", addTask).Methods("POST")
+	gRouter.HandleFunc("/updateform/{id}", getTaskUpdateForm)
+	gRouter.HandleFunc("/update/{id}", updateTask).Methods("PATCH")
+	gRouter.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
 
 	fmt.Println("Listening on Port 3030")
 	http.ListenAndServe(":3000", gRouter)
@@ -66,6 +71,82 @@ func fetchTasks(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Error fetching tasks: "+err.Error(), http.StatusInternalServerError)
 	}
+
+	tmpl.ExecuteTemplate(w, "todoList", tasks)
+}
+
+func newForm(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "taskAddForm", nil)
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	task := r.FormValue("task")
+
+	query := "INSERT INTO tasks (task) VALUES (?);"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, execErr := stmt.Exec(task)
+	if execErr != nil {
+		log.Fatal(err)
+	}
+
+	todos, _ := getTasks(db)
+
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func getTaskUpdateForm(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	taskId, _ := strconv.Atoi(vars["id"])
+
+	task, err := getTaskById(db, taskId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	tmpl.ExecuteTemplate(w, "taskUpdateForm", task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	taskname := r.FormValue("task")
+	taskdone := r.FormValue("done")
+
+	var doneBool bool
+
+	switch strings.ToLower(taskdone) {
+	case "yes", "on":
+		doneBool = true
+	default:
+		doneBool = false
+	}
+
+	task := Task{id, taskname, doneBool}
+
+	err := updateTaskById(db, &task)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	todos, _ := getTasks(db)
+
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	deleteTaskById(db, id)
+
+	tasks, _ := getTasks(db)
 
 	tmpl.ExecuteTemplate(w, "todoList", tasks)
 }
@@ -98,27 +179,61 @@ func getTasks(dbPointer *sql.DB) ([]Task, error) {
 	return tasks, nil
 }
 
-func newForm(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "taskAddForm", nil)
+func getTaskById(dbPointer *sql.DB, id int) (*Task, error) {
+	query := "SELECT id, task, done FROM tasks WHERE id = ?;"
+
+	var task Task
+
+	row := dbPointer.QueryRow(query, id)
+
+	err := row.Scan(&task.Id, &task.TaskName, &task.Done)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("No task was found with id %d", id)
+		}
+
+		return nil, err
+	}
+
+	return &task, nil
 }
 
-func addTask(w http.ResponseWriter, r *http.Request) {
-	task := r.FormValue("task")
+func updateTaskById(dbPointer *sql.DB, task *Task) error {
+	query := "UPDATE tasks SET task=?, done=? WHERE id=?;"
 
-	query := "INSERT INTO tasks (task) VALUES (?);"
-
-	stmt, err := db.Prepare(query)
+	result, err := dbPointer.Exec(query, task.TaskName, task.Done, task.Id)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	_, execErr := stmt.Exec(task)
-	if execErr != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	todos, _ := getTasks(db)
+	rowsChanged, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
 
-	tmpl.ExecuteTemplate(w, "todoList", todos)
+	if rowsChanged == 0 {
+		fmt.Println("No rows changed by update")
+	}
+
+	return nil
+}
+
+func deleteTaskById(dbPointer *sql.DB, id int) error {
+	query := "DELETE FROM tasks WHERE id=?;"
+
+	result, err := dbPointer.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsChanged, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsChanged == 0 {
+		fmt.Println("No rows deleted")
+	}
+
+	return nil
 }
